d421f68b4862fabb06ff4ba19914cdd31be47959
Jimmy Patel <jpatel@scu.edu>
2015-11-07 12:36:11 -0800

    JS change

--- www/js/init.js
+++ www/js/init.js
@@ -5,3 +5,29 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+function initMap() {
+  var myLatlng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatlng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Click to zoom'
+  });
+/*
+  map.addListener('center_changed', function() {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(function() {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
+*/
+
+}
