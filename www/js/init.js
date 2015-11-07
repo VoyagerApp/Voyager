f60940a2031891de92dc1a0eb4e5c069ea0b1b70
Jimmy Patel <jpatel@scu.edu>
2015-11-07 13:56:45 -0800

    Joined Map, Logo, Form needs work

--- www/js/init.js
+++ www/js/init.js
@@ -7,19 +7,44 @@
 })(jQuery); // end of jQuery name space
 
 
+
+
+
 function initMap() {
-  var myLatlng = {lat: -25.363, lng: 131.044};
+  	var map;
+	map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: 37.77, lng: -122.42},
+	    zoom: 8
+	});
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatlng
-  });
+	// Try HTML5 geolocation.
+	if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	      var pos = {
+	        lat: position.coords.latitude,
+	        lng: position.coords.longitude
+	      };
 
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    title: 'Click to zoom'
-  });
+	      map.setCenter(pos);
+	    });
+	}
+	else {
+	    // Browser doesn't support Geolocation
+	}
+
+	google.maps.event.addListener(map, 'click', function(e) {
+	    placeMarker(e.latLng, map);
+	});
+
+	function placeMarker(position, map) {
+	    var marker = new google.maps.Marker({
+	      position: position,
+	      map: map
+	    });  
+	    map.panTo(position);
+	}
+
+}
 /*
   map.addListener('center_changed', function() {
     // 3 seconds after the center of the map has changed, pan back to the
@@ -29,5 +54,3 @@ function initMap() {
     }, 3000);
   });
 */
-
-}
