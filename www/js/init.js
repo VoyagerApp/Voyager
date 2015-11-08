a54400097b52940dda7ac86af454b9690b368eef
Jimmy Patel <jpatel@scu.edu>
2015-11-07 16:46:11 -0800

    Need to add pins still

--- www/js/init.js
+++ www/js/init.js
@@ -7,11 +7,11 @@
 })(jQuery); // end of jQuery name space
 
 
-
+var map;
+var markers=[];
 
 
 function initMap() {
-  	var map;
 	map = new google.maps.Map(document.getElementById('map'), {
 	    center: {lat: 37.77, lng: -122.42},
 	    zoom: 8
@@ -33,17 +33,46 @@ function initMap() {
 	}
 
 	google.maps.event.addListener(map, 'click', function(e) {
-	    placeMarker(e.latLng, map);
+	    addMarker(e.latLng);
+	});
+}
+
+function addMarker(location) {
+	  var marker = new google.maps.Marker({
+	    position: location,
+	    map: map
+	  });
+	  markers.push(marker);
+	  setMapOnAll();
+}
+
+function btnaddmarker(){
+	var myLatLng1 = {lat: -25.363, lng: 131.044};
+	var myLatLng2 = {lat: -25.363, lng: 10.044};
+  	var marker1 = new google.maps.Marker({
+	    position: myLatLng1,
+	    map: map
+	});
+	var marker2 = new google.maps.Marker({
+	    position: myLatLng2,
+	    map: map
 	});
 
-	function placeMarker(position, map) {
-	    var marker = new google.maps.Marker({
-	      position: position,
-	      map: map
-	    });  
-	}
-
+	markers.push(marker1);
+	markers.push(marker2);
+	setMapOnAll();
 }
+
+function setMapOnAll() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+    alert(markers[i]);
+    console.log(markers[i].position.lat);
+  }
+}
+
+
+
 /*
   map.addListener('center_changed', function() {
     // 3 seconds after the center of the map has changed, pan back to the
@@ -52,4 +81,19 @@ function initMap() {
       map.panTo(marker.getPosition());
     }, 3000);
   });
+
+
+function dropFlightPathPins(){
+	var marker = new google.maps.Marker({
+	      position: new google.maps.LatLng(-122.379128, 37.623891),
+	      map: map
+	});
+
+	var marker = new google.maps.Marker({
+	      position: new google.maps.LatLng(55.365705, 25.253398),
+	      map: map
+	});
+
+}
 */
+
